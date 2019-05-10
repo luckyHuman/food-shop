0c926bd97dfba8a31456d72fe17a72e267577757
luckyHuman <gpclucky@163.com>
2019-05-10 10:52:27 +0800

    666

---
+++ src/components/AlertTip/AlertTip.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AlertTip"
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
